22110419a2042b3c578ff0137b4a4869943b9ac0
CrumbGregory <yanguzovg@mail.ru>
2021-10-18 19:17:19 +0500

    edit grid

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,7 +1,6 @@
 import Swiper from '../../node_modules/swiper/swiper-bundle';
 
 const swiperTeam = new Swiper('.slider-team', {
-    loop: true,
   
     navigation: {
       nextEl: '.slider-team__button--next',
